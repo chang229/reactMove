d2bb914056d18232e752d584c7883c8a5c74d9f1
changhongda <963515608@qq.com>
2019-07-14 18:06:46 +0800

    搜索

--- src/component/search/index.js
+++ src/component/search/index.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { SearchBar } from 'antd-mobile';
+import { _get } from '../../config/axios';
+import './index.css';
 
-class Search extends Component {
-    render(){
-        return (
-            <div>search</div>
-        )
-    }
+function Search() {
+
+    const [val,setVal] = useState('');
+
+    useEffect(() => {
+        _get('/movie/search',{params:{q:val}}).then((res) => {
+            console.log(res);
+        })
+        console.log(val);
+    })
+
+    return (
+        <div className="searchBox">
+            <div className="searchBack">
+                <Link to="/">关闭</Link>
+            </div>
+            <div className="searchIpt">
+                <SearchBar 
+                    onSubmit={value => setVal(value)}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Search;
